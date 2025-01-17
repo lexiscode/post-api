d541411955213b7077d9c2cff056dc46c70376a3
Nwokorie Alex V <nwokoriealex20@gmail.com>
2025-01-17 16:54:10 +0100

    feat: created crud for post api with authentication

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StorePostRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class PostController extends Controller
+{
+    use HttpResponses;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return PostResource::collection(
+            Post::where('user_id', Auth::user()->id)->get()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        $request->validated($request->all());
+
+        $post = Post::create([
+            'user_id' => Auth::user()->id,
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            return $this->isNotAuthorized($post) ? $this->isNotAuthorized($post) : new PostResource($post);
+        } catch (ModelNotFoundException $e) {
+            return $this->error('', 'Post not found', 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        if ($this->isNotAuthorized($post)) {
+            return $this->isNotAuthorized($post);
+        }
+        $post->update($request->all());
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        if ($this->isNotAuthorized($post)) {
+            return $this->isNotAuthorized($post);
+        }
+        $post->delete();
+        return $this->success(null, 'This post has been deleted successfully', 200);
+    }
+
+
+    private function isNotAuthorized($post)
+    {
+        if (Auth::user()->id !== $post->user_id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::resource('/posts', PostController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
